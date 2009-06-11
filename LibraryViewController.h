fc12c69340242e51f09558b092089c574525e9a5
Ian Terrell <ian.terrell@gmail.com>
2009-06-11 11:08:02 -0400

    initial library table view

---
+++ LibraryViewController.h
@@ -0,0 +1,15 @@
+//
+//  LibraryViewController.h
+//  Scarab
+//
+//  Created by Ian Terrell on 6/11/09.
+//  Copyright 2009 Ian Terrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LibraryViewController : UITableViewController {
+
+}
+
+@end
