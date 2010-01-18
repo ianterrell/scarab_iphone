1c3113a549bd52c6b29fa513aabcdd25339fffaa
Ian Terrell <ian.terrell@gmail.com>
2010-01-17 20:28:34 -0500

    updated copyright date

--- CreditsViewController.m
+++ CreditsViewController.m
@@ -16,7 +16,7 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
   [super viewDidLoad];
-  [UIHelpers addCopy:@"Scarab is thankful for generous artists with open licenses. Some tab bar icons were created by <a href=\"http://www.glyphish.com\">Glyphish</a> and are licensed under Creative Commons Attribution 3.0.  The star icons for favoriting works were created by <a href=\"http://www.icojoy.com\">Icojoy</a> and are <a href=\"http://www.freeiconsdownload.com/Free_Downloads.asp?id=265\">available</a> under the Creative Commons Attribution 3.0 license.  The download icon was created by <a href=\"http://pixel-mixer.com/\">Pixel-Mixer</a> and is <a href=\"http://www.iconspedia.com/icon/down-11614.html\">available</a> free for use.\n\nScarab is a publication of Old Brick Press, LLC. © Copyright 2009.  All rights reserved." toScrollView:scrollView];
+  [UIHelpers addCopy:@"Scarab is thankful for generous artists with open licenses. Some tab bar icons were created by <a href=\"http://www.glyphish.com\">Glyphish</a> and are licensed under Creative Commons Attribution 3.0.  The star icons for favoriting works were created by <a href=\"http://www.icojoy.com\">Icojoy</a> and are <a href=\"http://www.freeiconsdownload.com/Free_Downloads.asp?id=265\">available</a> under the Creative Commons Attribution 3.0 license.  The download icon was created by <a href=\"http://pixel-mixer.com/\">Pixel-Mixer</a> and is <a href=\"http://www.iconspedia.com/icon/down-11614.html\">available</a> free for use.\n\nScarab is a publication of Old Brick Press, LLC. © Copyright 2009-2010.  All rights reserved." toScrollView:scrollView];
 }
 
 - (id)init {
